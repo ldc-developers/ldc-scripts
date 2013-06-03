2d520da6be235a9b9ffb5cf5885f81c60b514eaa
David Nadlinger <code@klickverbot.at>
2013-06-03 03:14:22 +0200

    MinGW package creation adaptions.

--- ldc2-packaging/3-package-ldc.sh
+++ ldc2-packaging/3-package-ldc.sh
@@ -4,8 +4,17 @@
 
 cp $SRC_DIR/ldc/LICENSE $PKG_DIR
 
+if [ "$OS" == "mingw" ]; then
+    cp /local/bin/libconfig++-9.dll $PKG_DIR/bin
+fi
+
 cd $BUILD_ROOT
 mv $PKG_DIR $PKG_BASE
-tar czvf $PKG_BASE.tar.gz $PKG_BASE
-tar cvf $PKG_BASE.tar $PKG_BASE
-xz -z -e -9 $PKG_BASE.tar
+
+if [ "$OS" == "mingw" ]; then
+    7z a -t7z $PKG_BASE.7z $PKG_BASE -mx9
+else
+    tar czvf $PKG_BASE.tar.gz $PKG_BASE
+    tar cvf $PKG_BASE.tar $PKG_BASE
+    xz -z -e -9 $PKG_BASE.tar
+fi
