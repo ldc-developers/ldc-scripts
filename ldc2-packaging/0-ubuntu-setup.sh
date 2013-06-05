a5354b34aedc1bf6e12e5d21bccd1e1f90c17bbd
David Nadlinger <code@klickverbot.at>
2013-06-05 18:49:54 +0200

    Do not include env.sh in Ubuntu setup script.
    The instance will be rebooted in between anyway.

--- ldc2-packaging/0-ubuntu-setup.sh
+++ ldc2-packaging/0-ubuntu-setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export OS=linux
-export BUILD_ROOT=/build
-. env.sh
+set -e
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install subversion git-core g++-multilib make cmake libconfig++8-dev xz-utils s3cmd
 
+BUILD_ROOT=/build
+echo "Preparing ${BUILD_ROOT} as build root directory."
 sudo mkdir $BUILD_ROOT
 sudo chown ubuntu:ubuntu $BUILD_ROOT
