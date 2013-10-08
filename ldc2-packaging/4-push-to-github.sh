23489ab9debb03d994833cc2772d5ab031939cd2
David Nadlinger <code@klickverbot.at>
2013-10-08 19:47:57 +0200

    Added script to push packages to new GitHub Releases store.

---
+++ ldc2-packaging/4-push-to-github.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ -z "$GITHUB_API_TOKEN" ]; then
+    echo "Set GITHUB_API_TOKEN to a GitHub OAuth token authorized to access ldc-developers/ldc."
+    exit 1
+fi
+
+if [ -z "$GITHUB_RELEASE_ID" ]; then
+    cat <<-EOM
+Set GITHUB_RELEASE_ID to the GitHub-internal ID of the release in question.
+
+You can fetch a list of all releases by running:
+
+curl -i -H "Authorization: token \${GITHUB_API_TOKEN}" \\
+    -H "Accept: application/vnd.github.manifold-preview" \\
+    "https://api.github.com/repos/ldc-developers/ldc/releases"
+EOM
+    exit 1
+fi
+
+. env.sh
+
+curl -H "Authorization: token ${GITHUB_API_TOKEN}" \
+     -H "Accept: application/vnd.github.manifold-preview" \
+     -H "Content-Type: application/octet-stream" \
+     --data-binary $BUILD_ROOT/$PKG_BASE.tar.gz \
+     "https://uploads.github.com/repos/ldc-developers/ldc/releases/${GITHUB_RELEASE_ID}/assets?name=$PKG_BASE.tar.gz"
+
+curl -H "Authorization: token ${GITHUB_API_TOKEN}" \
+     -H "Accept: application/vnd.github.manifold-preview" \
+     -H "Content-Type: application/octet-stream" \
+     --data-binary $BUILD_ROOT/$PKG_BASE.tar.xz \
+     "https://uploads.github.com/repos/ldc-developers/ldc/releases/${GITHUB_RELEASE_ID}/assets?name=$PKG_BASE.tar.xz"
