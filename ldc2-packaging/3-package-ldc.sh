0a9ce1dcc1eed5fec7fb5d207861562b3e6602a3
Kai Nacke <kai@redstar.de>
2016-05-14 14:09:24 +0200

    Remove peviously build files first.

--- ldc2-packaging/3-package-ldc.sh
+++ ldc2-packaging/3-package-ldc.sh
@@ -15,16 +15,20 @@ if [ "$OS" == "mingw" ]; then
 fi
 
 cd $BUILD_ROOT
+rm -rf $PKG_BASE
 mv $PKG_DIR $PKG_BASE
 
 if [ "$OS" == "mingw" ]; then
+    rm -f $PKG_BASE.zip $PKG_BASE.7z
     zip -9 -r $PKG_BASE.zip $PKG_BASE
     7z a -t7z $PKG_BASE.7z $PKG_BASE -mx9
 else
+    rm -f $PKG_BASE.tar.gz $PKG_BASE.tar.xz
     tar czvf $PKG_BASE.tar.gz $PKG_BASE
     tar cvf $PKG_BASE.tar $PKG_BASE
     xz -z -e -9 $PKG_BASE.tar
 fi
 
 cd $SRC_DIR
+rm -f ../ldc-$LDC_VERSION$LDC_VERSION_SUFFIX-src.tar.gz
 tar czf ../ldc-$LDC_VERSION$LDC_VERSION_SUFFIX-src.tar.gz --exclude-vcs --transform=s/ldc/ldc-$LDC_VERSION$LDC_VERSION_SUFFIX-src/ ldc
