95a7b887be0b16887e14a7817381d539d861187a
Martin <noone@nowhere.com>
2017-05-13 21:37:24 +0200

    Fix Docker release script (copying artifacts to host)
    Use `docker cp <container:path> <host path>`, which creates the files under
    the host user context and not the container's root user. It doesn't support
    wildcards unfortunately.
    The release and source archives are copied to the host's current directory
    after the container has run but before removing it.
    
    Also don't hardcode LDC_VERSION and -_SUFFIX in the script, but require them
    as host environment variables and forward them to the container. All other
    environment variables (BUILD_WITH_LTO, DUB_VERSION, ...) will be set to their
    default values in the container.

--- docker-build-release/build-release.sh
+++ docker-build-release/build-release.sh
@@ -1,30 +1,41 @@
 #!/bin/bash
 # Builds an LDC release inside our build host Docker container.
 
-set -euo pipefail
+set -eo pipefail
 
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
 if [ $# -ne 1 ]; then
 	echo "Usage: $0 PLATFORM"
-	echo "    PLATFORM is one of $(ls -dm ldc-builder-*/ | sed s:ldc-builder-::g | sed s:/::g)."
+	echo "    PLATFORM is one of $(ls -dm $DIR/ldc-builder-*/ | sed s:$DIR/ldc-builder-::g | sed s:/::g)."
 	exit 1
 fi
 IMAGE=dlangldc/ldc-builder-$1
 
+if [ -z "$LDC_VERSION" ]; then
+    cat <<EOM
+Set LDC_VERSION to the LDC version to build (e.g. '0.10.0').
+
+This will be used for determining the branch name to fetch.
+
+Use LDC_VERSION_SUFFIX to specify an additional suffix to append to the
+version as it appears in file names (e.g. '-beta1').
+EOM
+    exit 1
+fi
+
 set -x
 
 docker pull $IMAGE
-
-mkdir -p artifacts
-docker run --rm \
+docker run \
+	--name ldcBuilder \
 	-v $DIR/../ldc2-packaging:/ldc2-packaging \
-	-v artifacts:/artifacts \
-	-e LDC_VERSION=1.2.0 \
-	-e LDC_VERSION_SUFFIX=-beta2 \
+	-e LDC_VERSION=$LDC_VERSION \
+	-e LDC_VERSION_SUFFIX=$LDC_VERSION_SUFFIX \
 	$IMAGE \
 	sh -c "cd /ldc2-packaging && \
 	./2-build-ldc.sh && \
 	./2a-build-dub.sh && \
-	./3-package-ldc.sh && \
-	 cp /build/*z /artifacts"
+	./3-package-ldc.sh"
+docker cp ldcBuilder:/build/ldc2-$LDC_VERSION$LDC_VERSION_SUFFIX-$1.tar.xz .
+docker cp ldcBuilder:/build/ldc-$LDC_VERSION$LDC_VERSION_SUFFIX-src.tar.gz .
+docker rm ldcBuilder
