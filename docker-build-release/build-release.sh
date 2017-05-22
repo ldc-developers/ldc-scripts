2206cfbc542fad34668899a1fd27424a5b5bf637
David Nadlinger <code@klickverbot.at>
2017-05-22 02:03:15 +0100

    docker-build-release: Take version to build as command line arguments, use PID in container name
    Also makes sure the container is always removed, even if the build fails.
    
    Using the shell PID as part of the name allows building several releases
    in parallel.

--- docker-build-release/build-release.sh
+++ docker-build-release/build-release.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
 # Builds an LDC release inside our build host Docker container.
 
-set -eo pipefail
+set -eou pipefail
 
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-if [ $# -ne 1 ]; then
-	echo "Usage: $0 PLATFORM"
-	echo "    PLATFORM is one of $(ls -dm $DIR/ldc-builder-*/ | sed s:$DIR/ldc-builder-::g | sed s:/::g)."
+if [ $# -ne 2 -a $# -ne 3 ]; then
+	echo "Usage: $0 PLATFORM LDC_VERSION [LDC_VERSION_SUFFIX]"
+	echo "    PLATFORM is one of $(ls -dm $DIR/ldc-builder-*/ | tr -d '\n' | sed s:$DIR/ldc-builder-::g | sed s:/::g)."
 	exit 1
 fi
 IMAGE=dlangldc/ldc-builder-$1
-
-if [ -z "$LDC_VERSION" ]; then
-    cat <<EOM
-Set LDC_VERSION to the LDC version to build (e.g. '0.10.0').
-
-This will be used for determining the branch name to fetch.
-
-Use LDC_VERSION_SUFFIX to specify an additional suffix to append to the
-version as it appears in file names (e.g. '-beta1').
-EOM
-    exit 1
+LDC_VERSION=$2
+LDC_VERSION_SUFFIX=
+if [ $# -eq 3 ]; then
+	LDC_VERSION_SUFFIX=$3
 fi
 
-set -x
+function print_step {
+	echo "$(tput setaf 6)$1$(tput sgr0)"
+}
+CONTAINER_NAME=ldc_builder_$$
+print_step "$(tput bold)Building LDC $LDC_VERSION$LDC_VERSION_SUFFIX in container '$CONTAINER_NAME'..."
 
+print_step "Fetching image $IMAGE"
 docker pull $IMAGE
+
+function cleanup {
+	print_step "Removing container '$CONTAINER_NAME'"
+	docker rm $CONTAINER_NAME
+}
+trap cleanup EXIT
+
+print_step "Running release build scripts"
 docker run \
-	--name ldcBuilder \
+	--name $CONTAINER_NAME \
 	-v $DIR/../ldc2-packaging:/ldc2-packaging \
 	-e LDC_VERSION=$LDC_VERSION \
 	-e LDC_VERSION_SUFFIX=$LDC_VERSION_SUFFIX \
@@ -36,6 +42,7 @@ docker run \
 	./2-build-ldc.sh && \
 	./2a-build-dub.sh && \
 	./3-package-ldc.sh"
-docker cp ldcBuilder:/build/ldc2-$LDC_VERSION$LDC_VERSION_SUFFIX-$1.tar.xz .
-docker cp ldcBuilder:/build/ldc-$LDC_VERSION$LDC_VERSION_SUFFIX-src.tar.gz .
-docker rm ldcBuilder
+
+print_step "Copying over release archives"
+docker cp $CONTAINER_NAME:/build/ldc2-$LDC_VERSION$LDC_VERSION_SUFFIX-$1.tar.xz .
+docker cp $CONTAINER_NAME:/build/ldc-$LDC_VERSION$LDC_VERSION_SUFFIX-src.tar.gz .
